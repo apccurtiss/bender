05848ba705ae06210cc31f8a407ec0a258c33a64
Alexander Curtiss <apccurtiss@gmail.com>
2017-08-04 00:39:47 +0000

    Moved files around for better organization and tab autocomplete.

--- bender.py
+++ bender.py
@@ -1,14 +1,6 @@
-import re
-import json
-import random
-import logging
-from datetime import datetime
 from slackbot.bot import Bot
-from slackbot.bot import listen_to
-from slackbot.bot import respond_to
 
-from bender_commands import quotes
-from bender_commands import slap
+from plugins import quotes, slap
 
 if __name__ == "__main__":
     bot = Bot()
