9e870347617096547368ea47c6a38238a606f3a4
aowsenek <anya.owsenek@gmail.com>
2018-06-06 10:09:30 -0600

    Removed files and reorged better

--- bender.py
+++
@@ -1,7 +0,0 @@
-from slackbot.bot import Bot
-
-from plugins import quotes, slap, swearjar, diplomacy
-
-if __name__ == "__main__":
-    bot = Bot()
-    bot.run()
--- slackbot.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-from slackbot.bot import Bot
-def main():
-    bot = Bot()
-    bot.run()
-
-if __name__ == "__main__":
-    main()
